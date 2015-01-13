690fcd7fbd31307f3a2e49da85b565b00a709c2d
JR Conlin <jrconlin+git@gmail.com>
2015-01-12 21:05:05 -0800

    Added push to server

--- app/src/main/java/com/mozilla/simplepush/simplepushdemoapp/MainActivity.java
+++ app/src/main/java/com/mozilla/simplepush/simplepushdemoapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -23,20 +25,40 @@ import com.google.android.gms.common.ConnectionResult;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Locale;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.apache.http.Header;
+import org.apache.http.HeaderIterator;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.DefaultRequestDirector;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HTTP;
 import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.drafts.Draft_76;
+import org.java_websocket.drafts.Draft_17;
 import org.java_websocket.handshake.ServerHandshake;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONStringer;
+import org.json.JSONTokener;
 
 
 public class MainActivity extends ActionBarActivity {
 
     String SENDER_ID = "1009375523940";
+    String CHANNEL_ID = "abad1dea-0000-0000-0000-000000000000";
 
     static final String TAG = "SimplepushDemoApp";
     static final String APP = "com.mozilla.simplepush.simplepushdemoapp";
@@ -46,48 +68,50 @@ public class MainActivity extends ActionBarActivity {
     private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
 
     TextView mDisplay;
-    EditText editText;
+    EditText host_url;
+    EditText ping_data;
+    Button send_button;
+    Button connect_button;
     GoogleCloudMessaging gcm;
     AtomicInteger msgId = new AtomicInteger();
     Context context;
 
     String regid;
+    String PushEndpoint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mDisplay = (TextView) findViewById(R.id.display);
-        editText = (EditText) findViewById(R.id.editText);
+        host_url = (EditText) findViewById(R.id.host_edit);
+        ping_data = (EditText) findViewById(R.id.message);
+        send_button = (Button) findViewById(R.id.send);
+        connect_button = (Button) findViewById(R.id.connect);
 
         context = getApplicationContext();
         if (checkPlayServices()) {
             gcm = GoogleCloudMessaging.getInstance(this);
             regid = getRegistrationId(context);
-            if (regid.isEmpty()) {
-                registerInBackground();
-            }
-
         } else {
             Log.i(TAG, "No valid Google Play Services APK found");
         }
 
-        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        host_url.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 boolean handled = false;
                 if (actionId == EditorInfo.IME_ACTION_SEND ||
                         (actionId == EditorInfo.IME_ACTION_UNSPECIFIED &&
                         event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
-                    registerInBackground();
+//                    registerInBackground();
                     handled = true;
                 }
                 return handled;
             }
-        });
+        }
+        );
     }
-
-
 
     @Override
     protected void onResume() {
@@ -101,6 +125,7 @@ public class MainActivity extends ActionBarActivity {
             if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
                 GooglePlayServicesUtil.getErrorDialog(resultCode, this,
                         PLAY_SERVICES_RESOLUTION_REQUEST).show();
+                toggleConnectToSend(false);
 
             } else {
                 Log.i(TAG, "This device is not supported");
@@ -169,7 +194,12 @@ public class MainActivity extends ActionBarActivity {
 
     public void onClick(final View view) {
         if (view == findViewById(R.id.send)) {
-            new AsyncTask<Void, Void, String>() {
+            String data = getMessage();
+            if (data.length() > 0) {
+                SendNotification(data);
+            }
+
+/*            new AsyncTask<Void, Void, String>() {
                 @Override
             protected String doInBackground(Void... params) {
                     String msg="";
@@ -188,9 +218,10 @@ public class MainActivity extends ActionBarActivity {
                 }
                 @Override
             protected void onPostExecute(String msg) {
-                    mDisplay.append(msg + "\n");
+                    mDisplay.setText(msg + "\n");
                 }
             }.execute(null, null, null);
+            */
         } else if (view == findViewById(R.id.clear)) {
             mDisplay.setText("");
         } else if (view == findViewById(R.id.connect)) {
@@ -199,8 +230,59 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
+    private void SendNotification(final String data) {
+        if (PushEndpoint.length() == 0) {
+            return;
+        }
+        new AsyncTask<String, Void, Boolean>() {
+            @Override
+        protected Boolean doInBackground(String... params) {
+        HttpPut req = new HttpPut(PushEndpoint);
+        HttpParams rparams = new BasicHttpParams();
+        rparams.setLongParameter("version", System.currentTimeMillis());
+        rparams.setParameter("data", data);
+        try {
+            StringEntity entity = new StringEntity(params.toString());
+            entity.setContentType("application/x-www-form-urlencoded");
+            entity.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE,
+                    "text/plain;charset=UTF-8"));
+            req.setEntity(entity);
+            DefaultHttpClient client = new DefaultHttpClient();
+            HttpResponse resp = client.execute(req);
+            int code = resp.getStatusLine().getStatusCode();
+            if ( code >= 200 && code < 300) {
+                return true;
+            }
+        } catch (ClientProtocolException x) {
+            Log.e(TAG, "Could not send Notification " + x);
+        } catch (IOException x) {
+            Log.e(TAG, "Could not send Notification (io) " + x);
+        } catch (Exception e) {
+            Log.e(TAG, "flaming crapsticks", e);
+        }
+        return false;
+            }
+
+            @Override
+        protected void onPostExecute(Boolean success) {
+                String msg = "was";
+                if (!success) {
+                    msg = "was not";
+                }
+                mDisplay.setText("Message " + msg + " sent");
+            }
+        }.execute(data, null, null);
+    }
+
+    private String getMessage() {
+        String target = ping_data.getText().toString();
+        if (target.length() > 0) {
+            Log.i(TAG, "Ping data set to " + target);
+        }
+        return target;
+    }
     private String getTarget() {
-        String target = editText.getText().toString();
+        String target = host_url.getText().toString();
         Log.i(TAG, "Setting target to " + target);
         return target;
     }
@@ -226,19 +308,52 @@ public class MainActivity extends ActionBarActivity {
         return getSharedPreferences(MainActivity.class.getSimpleName(), Context.MODE_PRIVATE);
     }
 
-    private void sendRegistrationIdToBackend(String regid) {
-        //TODO: Send the regId out.
+    void toggleConnectToSend(boolean state) {
+        host_url.setEnabled(!state);
+        connect_button.setEnabled(!state);
+        ping_data.setEnabled(state);
+        send_button.setEnabled(state);
+    }
+
+    private void sendRegistrationIdToBackend(final String regid) {
         String target = getTarget();
         Log.i(TAG, "Sending out Registration message for RegId: " + regid + " to " + target);
         try {
-            //TODO: FINISH THIS!
-            WebSocketClient ws = new WebSocketClient(new URI(target), new Draft_76()) {
+            URI uri = new URI(target);
+            WebSocketClient ws = new WebSocketClient(uri, new Draft_17()) {
                 private String TAG = "WEBSOCKET";
 
                 @Override
                 public void onMessage(String message) {
-                    Log.i(TAG, "message got:" + message + "\n");
-                    // On Hello, send register + proping content
+                    Log.i(TAG, "got message:" + message + "\n");
+                    if (message.equals("{}")) {
+                        Log.i(TAG, "Pong...");
+                        return;
+                    }
+                    try {
+                        JSONObject msg = new JSONObject(new JSONTokener(message));
+                        String msgType = msg.getString("messageType");
+                        switch (msgType) {
+                            case "hello":
+                                Log.i(TAG, "Sending registration message");
+                                JSONObject regObj = new JSONObject();
+                                regObj.put("channelID", CHANNEL_ID);
+                                regObj.put("messageType", "register");
+                                this.send(regObj.toString());
+                                break;
+                            case "register":
+                                PushEndpoint = msg.getString("pushEndpoint");
+                                String txt = "Registration successful: " +
+                                        PushEndpoint;
+                                mDisplay.setText(txt);
+                                //toggleConnectToSend(true);
+                                break;
+                            default:
+                                Log.e(TAG, "Unknown message type " + msgType);
+                        }
+                    }catch (JSONException x) {
+                        Log.e(TAG, "Could not parse message " + x);
+                    }
                     // On register, store response, close socket, return to regular broadcasting.
                 }
 
@@ -247,11 +362,14 @@ public class MainActivity extends ActionBarActivity {
                     Log.i(TAG, "handshake with: " + getURI());
                     try {
                         JSONObject json = new JSONObject();
+                        JSONObject connect = new JSONObject();
+                        connect.put("regid", regid);
                         json.put("messageType", "hello");
                         json.put("uaid", "");
                         json.put("channelIDs", new JSONArray());
+                        json.put("connect", connect);
                         Log.i(TAG, "Sending object: " + json.toString());
-                        // this.send(json.toString());
+                        this.send(json.toString());
                     }catch (JSONException ex) {
                         Log.e(TAG, "JSON Exception: " + ex);
                     }
@@ -260,6 +378,8 @@ public class MainActivity extends ActionBarActivity {
                 @Override
                 public void onClose(int code, String reason, boolean remote) {
                     Log.i(TAG, "Disconnected! " + getURI() + " Code:" + code + " " + reason + "\n");
+                    mDisplay.setText("Disconnected from server");
+                    toggleConnectToSend(false);
                 }
 
                 @Override
@@ -267,6 +387,7 @@ public class MainActivity extends ActionBarActivity {
                     Log.e(TAG, "### EXCEPTION: " + ex + "\n");
                 }
             };
+            ws.connect();
         }catch (URISyntaxException ex) {
             Log.e(TAG, "Bad URL for websocket.");
         }
